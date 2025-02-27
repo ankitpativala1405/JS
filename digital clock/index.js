982de4a35a91ac513b5f9930e30fef9c54e61da2
ankitpativala1405 <ankitj1405@gmail.com>
2025-02-27 19:53:54 +0530

    digital clock

--- digital clock/index.js
+++ digital clock/index.js
@@ -1,11 +1,28 @@
 setInterval(() => {
   let date = new Date();
 
+  let hour=date.getHours()
+  if(hour>=12){
+    innerhtmlampm="PM"
+  }
+  else{
+    innerhtmlampm="AM"
+  }
+
+  if(hour>12){
+    innerhtmlhour=date.getHours()-12
+  }
+  else{
+    innerhtmlhour=date.getHours()
+  }
+
   document.getElementById("shawdate").innerHTML = date.getDate();
   document.getElementById("shawmonth").innerHTML = date.getMonth() + 1;
   document.getElementById("shawyear").innerHTML = date.getFullYear();
-  document.getElementById("shawhour").innerHTML = date.getHours();
+  document.getElementById("shawhour").innerHTML = innerhtmlhour;
   document.getElementById("shawminutes").innerHTML = date.getMinutes();
   document.getElementById("shawseconds").innerHTML = date.getSeconds();
   document.getElementById("shawmiliseconds").innerHTML = date.getMilliseconds();
+  document.getElementById("shawampm").innerHTML = innerhtmlampm;
+
 });
