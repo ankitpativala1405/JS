b121c9b1f3f8f7e679dcb5ac377734a108eff220
ankitpativala1405 <ankitj1405@gmail.com>
2025-04-17 11:50:57 +0530

    maibn

---
+++ slider/indec.js
@@ -0,0 +1,101 @@
+
+//project 1
+console.log("Red and White \nA Transformation in Education")
+
+//project 2
+let name = "ankit"
+let age = 24
+
+console.log(name,age)
+console.log(typeof name,typeof age);
+
+//project 3
+let x=10    
+let y=20
+console.log(x,y);
+
+//project 4
+let a=1
+let b=2
+let c=3
+console.log((a*a)+(b*b)+(c*c))
+
+//project 5
+let d=2
+console.log(d*d*d)
+
+let num=2
+let cube=num**3
+console.log(cube)
+
+
+console.log(d**3)
+
+//project 6
+let e=1
+let f=2
+let g=3
+let h=4
+let i=5
+//sum=15
+console.log((e+e)+(f+f)+(g+g)+(h+h)+(i+i))
+console.log((e+f+g+h+i)*2);
+
+//project 7
+let j=1
+let k=2
+let l=3
+let m=4
+let n=5
+let o=6
+let p=7
+console.log((j+k+l)*(m+n+o+p))
+let q=j+k+l
+let r=m+n+o+p
+console.log(q*r)
+
+//project 8
+let s=100
+let t=200
+let u=31
+console.log(((s+t)%u))
+
+//project 9 
+
+let v=1
+let w=2
+let xa=3
+let ya=4
+
+let za=5    
+let aa=6    
+let ba=7
+
+console.log((za+aa+ba)-ya)
+
+//project 10
+
+let ca=7
+console.log(ca**2)
+
+//project 11
+let da=2
+ea=da*50
+console.log(ea)
+
+//project 12
+let fa=100
+let ga=200
+let ha=300
+let ia=400
+let ja=500
+ka=fa+ga+ha+ia+ja
+console.log("The sum of the variables is: " + ka);
+
+
+
+//project 13
+let la=4
+console.log(((la*3)+7)-10)
+
+
